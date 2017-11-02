5ac1c78c98b5c198d3bdff49339ceb7cdac3746d
miriam <miriam@mic.lan>
2017-11-02 20:43:26 +0000

    Swedish and Polish text files

---
+++ src/Collector/PolishCollector.java
@@ -0,0 +1,27 @@
+package Collector;
+
+import Utils.Creator;
+import Utils.Values;
+
+import java.util.List;
+import java.util.Map;
+
+public class PolishCollector extends WordsCollector {
+    // Order of context
+    private int order;
+    // Polish alphabet
+    private List<String> alphabet;
+    // List that counts the appearance of contexts in text
+    private Map<String, Values> context;
+    // List of combinations created in context
+    private List<String> contextCombinations;
+
+    public PolishCollector(String path, int order) {
+        super(path);
+        this.context = super.getContext();
+        this.contextCombinations = super.getContextCombinations();
+        this.alphabet = super.getAlphabet();
+        this.order = order;
+        Creator.loadWords(path, order, context, alphabet, contextCombinations);
+    }
+}
